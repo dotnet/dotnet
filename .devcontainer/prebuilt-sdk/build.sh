2938a5b30887d1b7b43f4dff04912cb3d1d5f924
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-09 17:48:53 +0100

    unset RepositoryName

--- .devcontainer/prebuilt-sdk/build.sh
+++ .devcontainer/prebuilt-sdk/build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+# GitHub Codespaces sets this and it conflicts with source-build scripts.
+unset RepositoryName
+
 source="${BASH_SOURCE[0]}"
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
 "$scriptroot"/../../prep.sh
-
-# GitHub Codespaces sets this and it conflicts with source-build scripts.
-unset RepositoryName
-
 "$scriptroot"/../../build.sh --online --clean-while-building || exit 0
