a663f012d059b23c6203a07329b2a3f0145abcc8
Rainer Sigwald <raines@microsoft.com>
2023-09-08 14:56:58 -0500

    [source-build-externals] Sync 52eb57b: MSBuildLocator 1.6.1 (#204)
    Original commit: https://github.com/dotnet/source-build-externals/commit/52eb57b87cf23aa97faedf8c9b9049110e664528
    
    [[ commit created by automation ]]

---
+++ src/source-build-externals/src/MSBuildLocator/src/MSBuildLocator/Utils/SemanticVersion.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Build.Locator
+{
+    internal class SemanticVersion : IComparable<SemanticVersion>
+    {
+        private readonly IEnumerable<string> _releaseLabels;
+        private Version _version;
+        private string _originalValue;
+
+        public SemanticVersion(Version version, IEnumerable<string> releaseLabels, string originalValue)
+        {
+            _version = version ?? throw new ArgumentNullException(nameof(version));
+
+            if (releaseLabels != null)
+            {
+                _releaseLabels = releaseLabels.ToArray();
+            }
+
+            _originalValue = originalValue;
+        }
+
+        /// <summary>
+        /// Major version X (X.y.z)
+        /// </summary>
+        public int Major => _version.Major;
+
+        /// <summary>
+        /// Minor version Y (x.Y.z)
+        /// </summary>
+        public int Minor => _version.Minor;
+
+        /// <summary>
+        /// Patch version Z (x.y.Z)
+        /// </summary>
+        public int Patch => _version.Build;
+
+        /// <summary>
+        /// A collection of pre-release labels attached to the version.
+        /// </summary>
+        public IEnumerable<string> ReleaseLabels => _releaseLabels ?? Enumerable.Empty<string>();
+
+        public string OriginalValue => _originalValue;
+
+        /// <summary>
+        /// The full pre-release label for the version.
+        /// </summary>
+        public string Release => _releaseLabels != null ? String.Join(".", _releaseLabels) : String.Empty;
+
+        /// <summary>
+        /// True if pre-release labels exist for the version.
+        /// </summary>
+        public bool IsPrerelease
+        {
+            get
+            {
+                if (ReleaseLabels != null)
+                {
+                    var enumerator = ReleaseLabels.GetEnumerator();
+                    return (enumerator.MoveNext() && !String.IsNullOrEmpty(enumerator.Current));
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Compare versions.
+        /// </summary>
+        public int CompareTo(SemanticVersion other)
+        {
+            return VersionComparer.Compare(this, other);
+        }
+    }
+}
