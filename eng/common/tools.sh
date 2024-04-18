7c3938051dcaf13715025541a1067438236ec990
Viktor Hofer <viktor.hofer@microsoft.com>
2024-04-18 13:09:14 +0200

    Update tools.sh

--- eng/common/tools.sh
+++ eng/common/tools.sh
@@ -505,14 +505,18 @@ function MSBuild-Core {
       # The build already logged an error, that's the reason it failed. Producing an error here only adds noise.
       echo "Build failed with exit code $exit_code. Check errors above."
 
+      echo "productBuild: $productBuild"
+      echo "properties: $properties"
       # When running on Azure Pipelines, override the returned exit code to avoid double logging.
       # Skip this when the build is a child of the VMR orchestrator build.
       if [[ "$ci" == true && -n ${SYSTEM_TEAMPROJECT:-} && "$product_build" != true && "$properties" != *"DotNetBuildRepo=true"* ]]; then
         Write-PipelineSetResult -result "Failed" -message "msbuild execution failed."
         # Exiting with an exit code causes the azure pipelines task to log yet another "noise" error
         # The above Write-PipelineSetResult will cause the task to be marked as failure without adding yet another error
+        echo "wrong exit code"
         ExitWithExitCode 0
       else
+        echo "right exit code $exit_code"
         ExitWithExitCode $exit_code
       fi
     }
