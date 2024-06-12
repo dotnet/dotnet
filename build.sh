68e2828942177a961b251d8a329a1aaf5b047537
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-06-12 10:02:22 +0300

    .

--- build.sh
+++ build.sh
@@ -287,8 +287,6 @@ source $scriptroot/eng/common/native/init-os-and-arch.sh
 source $scriptroot/eng/common/native/init-distro-rid.sh
 initDistroRidGlobal "$os" "$arch" ""
 if [[ -n "$target_rid" ]]; then
-  # override with user's preferred RID
-  export __DistroRid="$target_rid"
   properties+=( "/p:TargetRid=$target_rid" )
 fi
 
