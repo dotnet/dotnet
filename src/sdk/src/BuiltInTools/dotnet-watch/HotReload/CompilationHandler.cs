3f930eeb1fb4ea6078248f12ad237c17abb500f8
Tomas Matousek <tomat@microsoft.com>
2025-04-28 11:21:09 -0700

    Suppress obsolete warnings

--- src/sdk/src/BuiltInTools/dotnet-watch/HotReload/CompilationHandler.cs
+++ src/sdk/src/BuiltInTools/dotnet-watch/HotReload/CompilationHandler.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#pragma warning disable CS0618 // TODO: https://github.com/dotnet/sdk/pull/48752
+
 using System.Collections.Immutable;
 using System.Diagnostics;
 using Microsoft.Build.Graph;
