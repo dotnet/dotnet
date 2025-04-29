a66362c13679d7cc2ad77b665b870d690f55c9d8
Ella Hathaway <ellahathaway@microsoft.com>
2025-04-29 18:52:46 +0000

    Add license headers to remaining files

--- eng/tools/BuildComparer/IssueType.cs
+++ eng/tools/BuildComparer/IssueType.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 /// <summary>
 /// Defines types of issues that can be identified during asset comparison.
 /// </summary>
