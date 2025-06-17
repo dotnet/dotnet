a3534aff4451ec620210ddedb46b1c5ea4f5a5d7
Ella Hathaway <ellahathaway@microsoft.com>
2025-06-17 21:02:33 +0000

    Remove aspire

--- src/aspire/tests/Aspire.Confluent.Kafka.Tests/CommonHelpers.cs
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace Aspire.Confluent.Kafka.Tests;
-
-internal sealed class CommonHelpers
-{
-    public const string TestingEndpoint = "localhost:9092";
-}
