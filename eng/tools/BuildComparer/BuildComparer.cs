36db4fa3ea9163cf15dcd14ad4a2cd36c492cc3b
Matt Mitchell (.NET) <mmitche@microsoft.com>
2025-07-11 11:20:14 -0700

    Skip source build externals

--- eng/tools/BuildComparer/BuildComparer.cs
+++ eng/tools/BuildComparer/BuildComparer.cs
@@ -183,6 +183,7 @@ public abstract class BuildComparer
                 // These repos don't publish assets, so we skip them
                 if (baseDirectory.EndsWith("scenario-tests")
                     || baseDirectory.EndsWith("source-build-reference-packages")
+                    || baseDirectory.EndsWith("source-build-externals")
                     || baseDirectory.EndsWith("runtime")
                     || baseDirectory.EndsWith("sdk"))
                 {
