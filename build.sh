fe8dca2783ba70e2063d59582566300dddf81121
Michael Simons <msimons@microsoft.com>
2025-07-14 13:49:36 +0000

    Revert testing changes

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ echo "  --binaryLog                         Create MSBuild binary log (short: -b
   echo "  --arch, --target-arch <value>     Target architecture: e.g. x64, x86, arm64, arm, riscv64"
   echo "  --branding <preview|rtm|default>  Specify versioning for shipping packages/assets. 'preview' will produce assets suffixed with '.final', 'rtm' will not contain a pre-release suffix. Default or unspecified will use VMR repo defaults."
   echo "  --verbosity <value>               Msbuild verbosity: q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic] (short: -v)"
-  echo "  --with-system-libs <libs>         Use system versions of these libraries. Combine with a plus. 'all' will use all supported libraries. eg brotli+libunwind+rapidjson+zlib"
+  echo "  --with-system-libs <libs>         Use system versions of these libraries. Combine with a plus. 'all' will use all supported libraries. e.g. brotli+libunwind+rapidjson+zlib"
   echo ""
 
   echo "Actions:"
