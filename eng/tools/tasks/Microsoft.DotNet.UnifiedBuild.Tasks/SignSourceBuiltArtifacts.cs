0bf05ea0d8197c3a9d62a93146c47883fcb1bcb3
Ella Hathaway <ellahathaway@microsoft.com>
2025-08-27 22:53:22 +0000

    Use ProcessService

--- eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/SignSourceBuiltArtifacts.cs
+++ eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/SignSourceBuiltArtifacts.cs
@@ -7,13 +7,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Versioning;
+using System.Threading.Tasks;
 using Microsoft.Build.Framework;
+using Microsoft.DotNet.UnifiedBuild.Tasks.Services;
+using BuildTask = Microsoft.Build.Utilities.Task;
 
 [assembly:UnsupportedOSPlatform("windows")]
 
 namespace Microsoft.DotNet.UnifiedBuild.Tasks;
 
-public class SignSourceBuiltArtifacts : Microsoft.Build.Utilities.Task
+public class SignSourceBuiltArtifacts : BuildTask
 {
     /// <summary>
     /// List of source-built assets to sign
@@ -47,12 +50,14 @@ public class SignSourceBuiltArtifacts : Microsoft.Build.Utilities.Task
     private const string SignBinlogFileName = "sign-source-built-artifacts.binlog";
     private const int SigningTimeout = 60 * 60 * 1000 * 2; // 2 hours
 
-    public override bool Execute()
+    public override bool Execute() => ExecuteAsync().GetAwaiter().GetResult();
+
+    private async Task<bool> ExecuteAsync()
     {
         try
         {
             PrepareForSigning();
-            RunSigning();
+            await RunSigningAsync();
             Log.LogMessage(MessageImportance.High, "Signing complete.");
         }
         catch (Exception ex)
@@ -90,88 +95,45 @@ public class SignSourceBuiltArtifacts : Microsoft.Build.Utilities.Task
     /// <summary>
     /// Runs the signing process
     /// </summary>
-    private void RunSigning()
+    private async Task RunSigningAsync()
     {
-        using (var process = new Process())
+        (string command, string arguments) = GetSigningCommandAndArguments();
+        Log.LogMessage(MessageImportance.High, $"Running signing command: {command} {arguments}");
+
+        var processService = new ProcessService(Log, timeout: SigningTimeout);
+        ProcessResult result = await processService.RunProcessAsync(
+            command,
+            arguments,
+            environmentVariables: new List<ProcessEnvironmentVariable> { new ProcessEnvironmentVariable("OPENSSL_ENABLE_SHA1_SIGNATURES", "1") },
+            printOutput: true);
+
+        if (result.ExitCode != 0)
         {
-            (string command, List<string> arguments) = GetSigningCommandAndArguments();
-
-            process.StartInfo = new ProcessStartInfo()
-            {
-                FileName = command,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                EnvironmentVariables = { ["OPENSSL_ENABLE_SHA1_SIGNATURES"] = "1" }
-            };
-
-            foreach (string arg in arguments)
-            {
-                process.StartInfo.ArgumentList.Add(arg);
-            }
-
-            process.OutputDataReceived += (sender, args) =>
-            {
-                if (!string.IsNullOrEmpty(args.Data))
-                    Log.LogMessage(MessageImportance.High, args.Data);
-            };
-            process.ErrorDataReceived += (sender, args) =>
-            {
-                if (!string.IsNullOrEmpty(args.Data))
-                    Log.LogError(args.Data);
-            };
-
-            Log.LogMessage(MessageImportance.High, $"Running signing...");
-
-            process.Start();
-
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-
-            bool hasExited = process.WaitForExit(SigningTimeout);
-            if (!hasExited)
-            {
-                process.Kill();
-                process.WaitForExit();
-                throw new TimeoutException($"Signing timed out after {SigningTimeout / 1000} seconds.");
-            }
-
-            if (process.ExitCode != 0)
-            {
-                throw new Exception($"Signing failed with exit code {process.ExitCode}");
-            }
-
-            Log.LogMessage(MessageImportance.High, $"Signing completed.");
+            Log.LogError($"Signing failed with exit code {result.ExitCode}");
         }
     }
 
     /// <summary>
     /// Gets the command and arguments to run signing
     /// </summary>
-    private (string command, List<string> arguments) GetSigningCommandAndArguments()
+    private (string command, string arguments) GetSigningCommandAndArguments()
     {
         string buildScript = Path.Combine(DotNetEngDirectory, "common", "build.sh");
         string binlog = Path.Combine(LogsDirectory, SignBinlogFileName);
         string signableAssets = string.Join(';', SignableSourceBuiltAssets.Select(a => a.ItemSpec));
 
-        var arguments = new List<string>
-        {
-            "--ci",
-            "--configuration", "Release",
-            "--restore",
-            "--projects", Config.EmptyProjectPath,
-            "--sign",
-            "--excludecibinarylog",
-            $"/bl:{binlog}",
-            $"/p:OfficialBuildId={OfficialBuildId}",
-            $"/p:AspNetCoreSnkPath={Config.AspNetCoreSnkPath}",
-            $"/p:SignableSourceBuiltAssets=\"{signableAssets}\""
-        };
-
-        if (!string.IsNullOrWhiteSpace(SignProperties))
-        {
-            arguments.AddRange(SignProperties.Split(' '));
-        }
+        string arguments =
+            $"--ci " +
+            $"--configuration Release " +
+            $"--restore " +
+            $"--projects {Config.EmptyProjectPath} " +
+            $"--sign " +
+            $"--excludecibinarylog " +
+            $"/bl:{binlog} " +
+            $"/p:OfficialBuildId={OfficialBuildId} " +
+            $"/p:AspNetCoreSnkPath={Config.AspNetCoreSnkPath} " +
+            $"/p:SignableSourceBuiltAssets=\\\"{signableAssets}\\\" " +
+            $"{SignProperties}";
 
         return (buildScript, arguments);
     }
