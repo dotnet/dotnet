48cd55b11bcdef2a7ab2e286eae9017268f6d18a
Matt Thalman <mthalman@microsoft.com>
2025-09-25 10:12:56 -0500

    Delete shared repos

--- src/efcore/test/EFCore.Specification.Tests/Query/Associations/ComplexProperties/ComplexPropertiesBulkUpdateTestBase.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace Microsoft.EntityFrameworkCore.Query.Associations.ComplexProperties;
-
-public abstract class ComplexPropertiesBulkUpdateTestBase<TFixture>(TFixture fixture)
-    : AssociationsBulkUpdateTestBase<TFixture>(fixture)
-    where TFixture : ComplexPropertiesFixtureBase, new();
