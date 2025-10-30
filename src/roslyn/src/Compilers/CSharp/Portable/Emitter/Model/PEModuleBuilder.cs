928cb5d543dfb124a07b2f56c891e20102cb9c11
Djuradj Kurepa <dkurepa@microsoft.com>
2025-10-30 11:40:33 +0100

    Reset roslyn to main

--- src/roslyn/src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
+++ src/roslyn/src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
@@ -67,7 +67,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Emit
 
         private EmbeddableAttributes GetNeedsGeneratedAttributesInternal()
         {
-            return (EmbeddableAttributes)_needsGeneratedAttributes | Compilation.GetNeedsGeneratedAttributes();
+            return (EmbeddableAttributes)_needsGeneratedAttributes | Compilation.GetNeedsGeneratedAttributes(freezeState: this is not PEDeltaAssemblyBuilder);
         }
 
         private void SetNeedsGeneratedAttributes(EmbeddableAttributes attributes)
@@ -384,8 +384,11 @@ namespace Microsoft.CodeAnalysis.CSharp.Emit
                                 switch (member.Kind)
                                 {
                                     case SymbolKind.NamedType:
-                                        if (!((NamedTypeSymbol)member).IsExtension) // Tracked by https://github.com/dotnet/roslyn/issues/78963 : Figure out what to do about extensions, if anything
+                                        if (!((NamedTypeSymbol)member).IsExtension)
                                         {
+                                            // Since this method is used only for WinMD native PDB generation, and 
+                                            // since in metadata extension blocks have different representation
+                                            // and none of their members have user code in them, skip the blocks.
                                             namespacesAndTypesToProcess.Push((NamespaceOrTypeSymbol)member);
                                         }
                                         break;
@@ -2089,13 +2092,25 @@ namespace Microsoft.CodeAnalysis.CSharp.Emit
                                                       diagnostics);
         }
 
-        internal NamedTypeSymbol EnsureInlineArrayTypeExists(SyntaxNode syntaxNode, SyntheticBoundNodeFactory factory, int arrayLength, DiagnosticBag diagnostics)
+        internal NamedTypeSymbol EnsureInlineArrayTypeExists(SyntaxNode syntaxNode, SyntheticBoundNodeFactory factory, int arrayLength, BindingDiagnosticBag diagnostics)
         {
             Debug.Assert(Compilation.Assembly.RuntimeSupportsInlineArrayTypes);
             Debug.Assert(arrayLength > 0);
+            Debug.Assert(diagnostics.DiagnosticBag is not null);
+
+            if (arrayLength is >= 2 and <= 16)
+            {
+                // In .NET 10 and up, the runtime provides InlineArrayN<T> types for N from 2 to 16.
+                var arrayWellKnownType = WellKnownType.System_Runtime_CompilerServices_InlineArray2 + (arrayLength - 2);
+                Debug.Assert(arrayWellKnownType is >= WellKnownType.System_Runtime_CompilerServices_InlineArray2 and <= WellKnownType.System_Runtime_CompilerServices_InlineArray16);
+                if (Binder.TryGetOptionalWellKnownType(Compilation, arrayWellKnownType, diagnostics, syntaxNode.Location, out var existingType))
+                {
+                    return existingType;
+                }
+            }
 
             string typeName = GeneratedNames.MakeSynthesizedInlineArrayName(arrayLength, CurrentGenerationOrdinal);
-            var privateImplClass = GetPrivateImplClass(syntaxNode, diagnostics).PrivateImplementationDetails;
+            var privateImplClass = GetPrivateImplClass(syntaxNode, diagnostics.DiagnosticBag).PrivateImplementationDetails;
             var typeAdapter = privateImplClass.GetSynthesizedType(typeName);
 
             if (typeAdapter is null)
