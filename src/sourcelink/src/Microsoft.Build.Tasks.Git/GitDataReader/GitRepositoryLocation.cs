75b8c5204d5ef4655f698322e0658dd7e862386f
Viktor Hofer <viktor.hofer@microsoft.com>
2025-11-14 14:01:49 +0100

    Add sourcelink release/10.0.2xx to the VMR (#3335)
    Co-authored-by: Přemek Vysoký <premek.vysoky@microsoft.com>

---
+++ src/sourcelink/src/Microsoft.Build.Tasks.Git/GitDataReader/GitRepositoryLocation.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the License.txt file in the project root for more information.
+
+using System;
+
+namespace Microsoft.Build.Tasks.Git
+{
+    internal readonly struct GitRepositoryLocation
+    {
+        /// <summary>
+        /// Normalized full path. OS specific directory separators.
+        /// </summary>
+        public readonly string GitDirectory { get; }
+
+        /// <summary>
+        /// Normalized full path. OS specific directory separators.
+        /// </summary>
+        public readonly string CommonDirectory { get; }
+
+        /// <summary>
+        /// Normalized full path. OS specific directory separators. Optional.
+        /// </summary>
+        public readonly string? WorkingDirectory { get; }
+
+        internal GitRepositoryLocation(string gitDirectory, string commonDirectory, string? workingDirectory)
+        {
+            NullableDebug.Assert(PathUtils.IsNormalized(gitDirectory));
+            NullableDebug.Assert(PathUtils.IsNormalized(commonDirectory));
+            NullableDebug.Assert(workingDirectory == null || PathUtils.IsNormalized(workingDirectory));
+
+            GitDirectory = gitDirectory;
+            CommonDirectory = commonDirectory;
+            WorkingDirectory = workingDirectory;
+        }
+    }
+}
---
+++ src/sourcelink/src/TestUtilities/ConditionalFactAttribute.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the License.txt file in the project root for more information.
+
+using System;
+using System.IO;
+using Xunit;
+
+namespace TestUtilities
+{
+    public class ConditionalFactAttribute : FactAttribute
+    {
+        public ConditionalFactAttribute(params Type[] skipConditions)
+        {
+            foreach (var skipCondition in skipConditions)
+            {
+                ExecutionCondition condition = (ExecutionCondition)Activator.CreateInstance(skipCondition)!;
+                if (condition.ShouldSkip)
+                {
+                    Skip = condition.SkipReason;
+                    break;
+                }
+            }
+        }
+    }
+
+    public class ConditionalTheoryAttribute : TheoryAttribute
+    {
+        public ConditionalTheoryAttribute(params Type[] skipConditions)
+        {
+            foreach (var skipCondition in skipConditions)
+            {
+                ExecutionCondition condition = (ExecutionCondition)Activator.CreateInstance(skipCondition)!;
+                if (condition.ShouldSkip)
+                {
+                    Skip = condition.SkipReason;
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public abstract class ExecutionCondition
+    {
+        public abstract bool ShouldSkip { get; }
+        public abstract string SkipReason { get; }
+    }
+
+    public class WindowsOnly : ExecutionCondition
+    {
+        public override bool ShouldSkip => Path.DirectorySeparatorChar != '\\';
+        public override string SkipReason => "Test not supported on Mac and Linux";
+    }
+
+    public class UnixOnly : ExecutionCondition
+    {
+        public override bool ShouldSkip => Path.DirectorySeparatorChar == '\\';
+        public override string SkipReason => "Test not supported on Windows";
+    }
+}
