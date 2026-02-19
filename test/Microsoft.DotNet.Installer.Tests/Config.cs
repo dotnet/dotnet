3381c725b713c5a787719144718179614ded33bd
Matt Thalman <mthalman@microsoft.com>
2026-02-19 15:01:52 -0600

    Handle internal package URLs for installer tests

--- test/Microsoft.DotNet.Installer.Tests/Config.cs
+++ test/Microsoft.DotNet.Installer.Tests/Config.cs
@@ -52,6 +52,9 @@ public static class Config
     public static string TargetFrameworkVersion { get; } = TryGetRuntimeConfig(TargetFrameworkVersionSwitch, out string? value) ? value : string.Empty;
     const string TargetFrameworkVersionSwitch = RuntimeConfigSwitchPrefix + nameof(TargetFrameworkVersion);
 
+    public static string DotNetRuntimeSourceFeedKey { get; } = TryGetRuntimeConfig(DotNetRuntimeSourceFeedKeySwitch, out string? value) ? value : string.Empty;
+    const string DotNetRuntimeSourceFeedKeySwitch = RuntimeConfigSwitchPrefix + nameof(DotNetRuntimeSourceFeedKey);
+
     public const string RuntimeConfigSwitchPrefix = "Microsoft.DotNet.Installer.Tests.";
 
     public static Architecture GetArchitecture(string architecture) => architecture switch
