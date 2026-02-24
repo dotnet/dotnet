4ada0784bbba9d3fdc20c82a7a397f783bbacae4
Přemek Vysoký <premek.vysoky@microsoft.com>
2026-02-24 10:26:24 +0100

    [release/10.0.3xx] Manual flow of dotnet/razor (#5039)
    Co-authored-by: David Wengier <git@wengier.com>

--- src/razor/src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/MarkupStartTagSyntax.cs
+++ src/razor/src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/MarkupStartTagSyntax.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.Razor.Language.Legacy;
-
 namespace Microsoft.AspNetCore.Razor.Language.Syntax;
 
 internal partial class MarkupStartTagSyntax
@@ -10,18 +8,6 @@ internal partial class MarkupStartTagSyntax
     public string GetTagNameWithOptionalBang()
     {
         return Name.IsMissing ? string.Empty : Bang.Content + Name.Content;
-    }
-
-    public bool IsSelfClosing()
-    {
-        return ForwardSlash.Kind != SyntaxKind.None &&
-            !ForwardSlash.IsMissing &&
-            !CloseAngle.IsMissing;
-    }
-
-    public bool IsVoidElement()
-    {
-        return ParserHelpers.VoidElements.Contains(Name.Content);
     }
 
     public override BaseMarkupEndTagSyntax? GetEndTag()
