4a5f24248289cafdc447822874f3f0976253607a
vsadov <8218165+VSadov@users.noreply.github.com>
2026-04-10 21:03:59 -0700

    128

--- src/runtime/src/libraries/System.Private.CoreLib/src/System/Threading/LowLevelLifoSemaphore.cs
+++ src/runtime/src/libraries/System.Private.CoreLib/src/System/Threading/LowLevelLifoSemaphore.cs
@@ -38,7 +38,7 @@ namespace System.Threading
         //    - in between we have a linear gain.
         //    all should be smoothed somewhat by the randomness of individual spin iterations.
 
-        private const int DefaultSemaphoreSpinCountLimit = 256;
+        private const int DefaultSemaphoreSpinCountLimit = 128;
 
         private CacheLineSeparatedCounts _separated;
 
