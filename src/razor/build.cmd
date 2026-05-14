a265f37d3d377a7c2385745c7d04ab71d4ad9a8e
David Wengier <git@wengier.com>
2026-05-14 12:30:31 +1000

    darc vmr remove-repo razor

--- src/razor/build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-set _args=-restore -build %*
-if "%~1"=="-?" set _args=-help
-
-powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0eng\common\build.ps1'" %_args%
-exit /b %ERRORLEVEL%
