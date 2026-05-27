c87221f7a37d622a8336af3b6bbba3165e6ab54a
Alexander Köplinger <alex.koeplinger@outlook.com>
2026-05-27 21:00:18 +0200

    Fix merge markers

--- src/sdk/src/Dotnet.Watch/Watch/Process/ProcessLaunchResult.cs
+++ src/sdk/src/Dotnet.Watch/Watch/Process/ProcessLaunchResult.cs
@@ -1,19 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-<<<<<<<< HEAD:src/sdk/src/Dotnet.Watch/Watch/Process/ProcessLaunchResult.cs
 namespace Microsoft.DotNet.Watch;
 
 internal sealed class ProcessLaunchResult
 {
     public int? ProcessId { get; set; }
-========
-using System.Text;
-
-namespace Microsoft.TemplateEngine.TestTemplates.test_templates.PostActions.RunScript.Basic
-{
-    class Class1
-    {
-    }
->>>>>>>> darc/forward/8ae9944-a357f65:src/sdk/test/TestAssets/TestPackages/TemplateEngine/test_templates/PostActions/Instructions/Basic/Class1.cs
 }
